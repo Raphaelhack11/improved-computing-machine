7441244bbafc389b0ada58ce12ef90d7cdfc28a2
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-08-26 19:31:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import LandingPage from "./pages/LandingPage";
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
-import SignupPage from "./pages/SignupPage";
-import LoginPage from "./pages/LoginPage";
-import DashboardPage from "./pages/DashboardPage";
-import DepositPage from "./pages/DepositPage";
-import WithdrawPage from "./pages/WithdrawPage";
-import HistoryPage from "./pages/HistoryPage";
-import AdminPage from "./pages/AdminPage";
-
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="pt-20"> {/* space for fixed navbar */}
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/deposit" element={<DepositPage />} />
-          <Route path="/withdraw" element={<WithdrawPage />} />
-          <Route path="/history" element={<HistoryPage />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="*" element={<div className="p-8 text-center text-xl">404 - Page Not Found</div>} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="bg-red-500 h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">Tailwind is working 🎉</h1>
+    </div>
   );
 }
+
+export default App;
