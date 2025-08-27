85e6ff25729402173d6f8fc8e275d227ade5ad42
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-08-27 08:14:12 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // ✅ Scan all React files
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
