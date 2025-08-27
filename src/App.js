d0dace3438e84a39fc6a7e9842e342774bf97457
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-08-27 14:18:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import ChatWidget from "./components/ChatWidget";
-import ScrollToTop from "./components/ScrollToTop";
-
 import LandingPage from "./pages/LandingPage";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import DashboardPage from "./pages/DashboardPage";
-import DepositPage from "./pages/DepositPage";
-import WithdrawPage from "./pages/WithdrawPage";
-import HistoryPage from "./pages/HistoryPage";
-import ContactPage from "./pages/ContactPage";
-import AdminDashboardPage from "./pages/AdminDashboard";
-import NotFound from "./pages/NotFound";
 
 export default function App() {
-  return (
-    <AuthProvider>
-      <BrowserRouter>
-        <ScrollToTop />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/deposit" element={<DepositPage />} />
-          <Route path="/withdraw" element={<WithdrawPage />} />
-          <Route path="/history" element={<HistoryPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-        <ChatWidget />
-      </BrowserRouter>
-    </AuthProvider>
-  );
+  return <LandingPage />;
     }
